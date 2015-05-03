4ec9ae3ad6b6625c9e5397b15aad46799ab21052
Miriam <mpark94@gmail.com>
2015-05-03 15:45:42 -0400

    first commit

--- config/env/development.js
+++ config/env/development.js
@@ -6,18 +6,18 @@ module.exports = {
 		title: 'MEANScoket - Development Environment'
 	},
 	facebook: {
-		clientID: process.env.FACEBOOK_ID || 'APP_ID',
-		clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
+		clientID: process.env.FACEBOOK_ID || '618789921588760',
+		clientSecret: process.env.FACEBOOK_SECRET || 'c13528108f7bf861ee5ba699cf41db70',
 		callbackURL: '/auth/facebook/callback'
 	},
 	twitter: {
-		clientID: process.env.TWITTER_KEY || 'CONSUMER_KEY',
-		clientSecret: process.env.TWITTER_SECRET || 'CONSUMER_SECRET',
+		clientID: process.env.TWITTER_KEY || 'zZy01TIgsSvC45S3dUHomIWvS',
+		clientSecret: process.env.TWITTER_SECRET || 'NI6ddaLr5m43VkL6RyIw3jnBXyUXyAWn9yt3T36rhDS85MeaAT',
 		callbackURL: '/auth/twitter/callback'
 	},
 	google: {
-		clientID: process.env.GOOGLE_ID || 'APP_ID',
-		clientSecret: process.env.GOOGLE_SECRET || 'APP_SECRET',
+		clientID: process.env.GOOGLE_ID || '229808432603-kha9ocvil34tiqkdrr4n0ft55nqc1gi3.apps.googleusercontent.com',
+		clientSecret: process.env.GOOGLE_SECRET || 'AYkNnj0O07j2KkkuaBWnQSh3',
 		callbackURL: '/auth/google/callback'
 	},
 	linkedin: {
